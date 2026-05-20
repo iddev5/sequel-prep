91ea6ba20078a253fde7afc7736945d06584cd65
iddev5 <ayushbardhan5@gmail.com>
2026-05-20 13:17:48 +0530

    Initial post specific page

---
+++ app/discuss/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import Navbar from "@/components/navbar";
+import { prisma } from "@/lib/prisma";
+
+export default async function Post({ params }) {
+  const { slug } = await params;
+  const postId = slug;
+
+  const post = await prisma.post.findUnique({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+    },
+    where: {
+      id: postId
+    }
+  });
+
+  return <div className="bg-primary w-full h-screen">
+    <Navbar />
+    <div className="flex w-full justify-center">
+      <div className="w-[45vw] p-8">
+        <h1 className="text-white text-2xl">{post.title}</h1>
+        <p className="text-white">{post.description}</p>
+      </div>
+    </div>
+  </div>
+}
